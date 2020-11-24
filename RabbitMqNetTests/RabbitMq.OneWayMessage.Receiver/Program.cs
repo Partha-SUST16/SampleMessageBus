1bfaf2bfefc1da98b4f68787c449b283712a5619
Partha-SUST16 <partha05@student.sust.edu>
2020-11-25 01:07:03 +0600

    receive event

--- RabbitMqNetTests/RabbitMq.OneWayMessage.Receiver/Program.cs
+++ RabbitMqNetTests/RabbitMq.OneWayMessage.Receiver/Program.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Text;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace RabbitMq.OneWayMessage.Receiver
 {
     class Program
     {
+        private static IModel channelForEventing;
         static void Main(string[] args)
         {
-            ReceiveSingleOneWayMessage();
+            ReceiveMessagesWithEvents();
         }
         private static void ReceiveSingleOneWayMessage()
         {
@@ -25,6 +29,35 @@ namespace RabbitMq.OneWayMessage.Receiver
             DefaultBasicConsumer basicConsumer = new OneWayMessageReceiver(channel);
             channel.BasicConsume("my.first.queue", false, basicConsumer);
 
+        }
+        private static void ReceiveMessagesWithEvents()
+        {
+            ConnectionFactory connectionFactory = new ConnectionFactory();
+
+            connectionFactory.Port = 5672;
+            connectionFactory.HostName = "localhost";
+            connectionFactory.UserName = "guest";
+            connectionFactory.Password = "guest";
+            connectionFactory.VirtualHost = "accounting";
+
+            IConnection connection = connectionFactory.CreateConnection();
+            channelForEventing = connection.CreateModel();
+            channelForEventing.BasicQos(0, 1, false);
+            EventingBasicConsumer eventingBasicConsumer = new EventingBasicConsumer(channelForEventing);
+            eventingBasicConsumer.Received += EventingBasicConsumer_Received;
+            channelForEventing.BasicConsume("my.first.queue", false, eventingBasicConsumer);
+
+        }
+        private static void EventingBasicConsumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            IBasicProperties basicProperties = e.BasicProperties;
+            Console.WriteLine("Message received by the event based consumer. Check the debug window for details.");
+            Debug.WriteLine(string.Concat("Message received from the exchange ", e.Exchange));
+            Debug.WriteLine(string.Concat("Content type: ", basicProperties.ContentType));
+            Debug.WriteLine(string.Concat("Consumer tag: ", e.ConsumerTag));
+            Debug.WriteLine(string.Concat("Delivery tag: ", e.DeliveryTag));
+            Debug.WriteLine(string.Concat("Message: ", e.Body.ToString()));
+            channelForEventing.BasicAck(e.DeliveryTag, false);
         }
     }
 }
