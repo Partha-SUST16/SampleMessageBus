10758d57b30b86641bed9b1d30e3ba82d4b3d6c0
Partha-SUST16 <partha05@student.sust.edu>
2020-11-25 00:38:41 +0600

    sends a message to queue

--- RabbitMqNetTests/RabbitMqNetTests/Program.cs
+++ RabbitMqNetTests/RabbitMqNetTests/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using RabbitMQ.Client;
 
 namespace RabbitMqNetTests
@@ -8,9 +9,33 @@ namespace RabbitMqNetTests
         static void Main(string[] args)
         {
             ConnectionFactory connectionFactory = new ConnectionFactory();
-            connectionFactory.Uri = new Uri("amqp://guest:guest@localhost:5672/accounting");
+
+            connectionFactory.Port = 5672;
+            connectionFactory.HostName = "localhost";
+            connectionFactory.UserName = "guest";
+            connectionFactory.Password = "guest";
+            connectionFactory.VirtualHost = "accounting";
+
             IConnection connection = connectionFactory.CreateConnection();
+            IModel channel = connection.CreateModel();
             Console.WriteLine(string.Concat("Connection open: ", connection.IsOpen));
+
+            channel.ExchangeDeclare("my.first.exchange", ExchangeType.Direct, true, false, null);
+            channel.QueueDeclare("my.first.queue", true, false, false, null);
+            channel.QueueBind("my.first.queue", "my.first.exchange", "");
+
+            IBasicProperties properties = channel.CreateBasicProperties();
+            properties.Persistent = true;
+            properties.ContentType = "text/plain";
+            PublicationAddress address = new PublicationAddress(ExchangeType.Direct, "my.first.exchange", "");
+            channel.BasicPublish(address, properties, Encoding.UTF8.GetBytes("This is a message from the RabbitMq .NET driver"));
+
+            channel.Close();
+            connection.Close();
+            Console.WriteLine(string.Concat("Channel is closed: ", channel.IsClosed));
+
+            Console.WriteLine("Main done...");
+            Console.ReadKey();
         }
     }
 }
