a061bfec43027a2947a700045c4e393da1782b09
Partha-SUST16 <partha05@student.sust.edu>
2020-11-25 00:34:17 +0600

    RabbitMQNetTests

---
+++ RabbitMqNetTests/RabbitMqNetTests/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using RabbitMQ.Client;
+
+namespace RabbitMqNetTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory connectionFactory = new ConnectionFactory();
+            connectionFactory.Uri = new Uri("amqp://guest:guest@localhost:5672/accounting");
+            IConnection connection = connectionFactory.CreateConnection();
+            Console.WriteLine(string.Concat("Connection open: ", connection.IsOpen));
+        }
+    }
+}
